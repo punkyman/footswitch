98c2b08c7fc1d8bef54535da72658eb50d2c538b
punkyman <djbiggouji@yahoo.fr>
2026-03-13 12:43:12 +0100

    setup simple serial communication to test the board

--- project/footswitch/src/main.cpp
+++ project/footswitch/src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+  Serial.println("Ping");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
