760ae99461de6b6edefdc6bf4ac52d53ca951f82
punkyman <djbiggouji@yahoo.fr>
2026-03-13 13:57:53 +0100

    simple buton press detection on pin 10

--- project/footswitch/src/main.cpp
+++ project/footswitch/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
+#define D10 A10 // as pins are the same on layout
+
+static bool s_buttonWasPressed = false;
+
 void setup() {
+  pinMode(D10, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(2000);
-  Serial.println("Ping");
+  bool value = digitalRead(D10);
+  if(value == LOW)
+  {
+    if(!s_buttonWasPressed)
+    {
+      s_buttonWasPressed = true;
+        Serial.println("Pressed");
+    }
+  }
+  else
+    s_buttonWasPressed = false;
 }
 
